e5073dbb26e2498a5a684da1be9da35f413b7fee
asdfyy <58731446+asdfyy@users.noreply.github.com>
2020-03-15 17:38:51 +0900

    a

---
+++ app/assets/javascripts/style.js
@@ -0,0 +1,54 @@
+$(function(){
+        $("#open").show();
+        $("#close").hide();
+    // アイコンをクリック
+	$("#open").click(function(){
+		// ulメニューを開閉する
+		$("#navi").slideToggle();
+        $("#open").hide();
+        $("#close").show();
+	});
+ 
+    // アイコンをクリック
+	$("#close").click(function(){
+		// ulメニューを開閉する
+		$("#navi").slideToggle();
+        $("#open").show();
+        $("#close").hide();
+	});
+});    
+
+
+
+$(function () {
+    var topBtn = $('#pagetop');
+    topBtn.hide();
+    //スクロールが300に達したらボタン表示
+    $(window).scroll(function () {
+        if ($(this).scrollTop() > 300) {
+            topBtn.fadeIn();
+        } else {
+            topBtn.fadeOut();
+        }
+    });
+    //スクロールでトップへもどる
+    topBtn.click(function () {
+        $('body,html').animate({
+            scrollTop: 0
+        }, 500);
+        return false;
+    });
+});
+
+$(function () {
+    $('.mainimg').slick({
+        dots: true,
+        fade: true,
+        autoplay: true,
+        autoplaySpeed: 4000,
+    });
+});
+
+$(document).ready(function() {
+    $("p").text("jQuery稼働テスト(稼働中)");
+});
